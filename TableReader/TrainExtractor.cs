ff0727b23d99e035d66fc72fea7b93b98b0fdf64
Kozinak <yroslavbot1@gmail.com>
2026-07-28 22:05:55 +0300

    Create TrainCounter function for TableReader

--- TableReader/TrainExtractor.cs
+++ TableReader/TrainExtractor.cs
@@ -21,6 +21,8 @@ class TrainExtractor
         var PaymentDataList = book.Worksheet(3);
         FirstRows[2] = GetFirstRowIndex(PaymentDataList);
         
+        Console.WriteLine(GetTrainCount(PassengerDataList, FirstRows[1]));
+        
         
     }
 
@@ -35,6 +37,20 @@ class TrainExtractor
             value = cell.Value.ToString();
         }
         return counter;
+    }
+
+    private int GetTrainCount(IXLWorksheet worksheet, int TableStart)
+    {
+        int counter = TableStart;
+        string value = "1";
+        string id = "1";
+        while (int.TryParse(id, out _) && int.TryParse(value, out _))
+        {
+            counter++;
+            id = worksheet.Cell(counter, 1).Value.ToString();
+            value = worksheet.Cell(counter, 2).Value.ToString().Split('/')[0];
+        }
+        return Convert.ToInt32(id)-1;
     }
 }
 
