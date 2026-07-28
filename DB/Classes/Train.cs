896976a4ae602b9ae542711f2bba6b9810768106
Kozinak <yroslavbot1@gmail.com>
2026-07-28 16:56:00 +0300

    Create Train class

---
+++ DB/Classes/Train.cs
@@ -0,0 +1,43 @@
+namespace DB;
+
+public class Train
+{
+    public int Id { get; set; }
+    public string Number { get; set; }
+    
+    //Тех. Данные
+    public string StationFrom { get; set; }
+    public string StationTo { get; set; }
+    
+    public int Distance { get; set; }
+    public int RailcarCount { get; set; }
+    public int DayInRaise
+    {
+        get { return DayInRaise;}
+        set
+        {
+            if (value > 0 && value < 31) DayInRaise = value;
+            else throw new ArgumentOutOfRangeException($"Неверное количество дней у поезда {this.Id} ({value})");
+        }
+    }
+    public int RangePerDay { get; }
+    public int RangePerMonth { get; }
+    
+    //Пассажиропоток
+    public PasCategory Casual { get; set; }
+    public PasCategory Student { get; set; }
+    public PasCategory FedBenefit { get; set; }
+    public PasCategory RegBenefit { get; set; }
+    public PasCategory Another { get; set; }
+    
+    
+    
+}
+
+public class PasCategory
+{
+    public int Count { get; set; }
+    public int Payment { get; set; }
+    public int WayLength { get; set; }
+    public int PaymentBySubject { get; set; }
+}
