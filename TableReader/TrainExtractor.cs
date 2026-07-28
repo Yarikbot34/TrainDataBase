ef7b5682c0f7b7394a60d1a7553d68cd7f5f5f60
Kozinak <yroslavbot1@gmail.com>
2026-07-28 21:33:45 +0300

    Create GetFirstRow Function

--- TableReader/TrainExtractor.cs
+++ TableReader/TrainExtractor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices.ComTypes;
 using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 
 namespace TableReader;
@@ -11,14 +12,29 @@ class TrainExtractor
         using var book = new XLWorkbook(fs);
         
         if (book.Worksheets.Count != 3) return;
-        
+
+        int[] FirstRows = new int[3];
         var TrainDataList = book.Worksheet(1);
+        FirstRows[0] = GetFirstRowIndex(TrainDataList);
         var PassengerDataList = book.Worksheet(2);
+        FirstRows[1] = GetFirstRowIndex(PassengerDataList);
         var PaymentDataList = book.Worksheet(3);
+        FirstRows[2] = GetFirstRowIndex(PaymentDataList);
         
         
-        
-        
+    }
+
+    private int GetFirstRowIndex(IXLWorksheet worksheet)
+    {
+        int counter = 0;
+        string value = "";
+        while (value != "1")
+        {
+            counter++;
+            IXLCell cell = worksheet.Cell(counter, 1);
+            value = cell.Value.ToString();
+        }
+        return counter;
     }
 }
 
