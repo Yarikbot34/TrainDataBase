54e08412146e98011548d265a00afe4931e2e0f0
Kozinak <yroslavbot1@gmail.com>
2026-08-08 09:28:31 +0300

    Create: Month data DTO class

---
+++ Domain/DTO/MonthDataDto.cs
@@ -0,0 +1,15 @@
+namespace Domain.DTO;
+
+public class MonthDataDto
+{
+    public int year { get; set; }
+    public int month { get; set; }
+    
+    public int CasualPayment { get; set; }
+    public int StudentPayment { get; set; }
+    public int FedBenefitPayment { get; set; }
+    public int RegBenefitPayment { get; set; }
+    
+    public int TrainKmPerMonth { get; set; }
+    
+}
