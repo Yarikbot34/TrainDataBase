013863cb0330b1a36bd73135c1389474a9cb9255
Kozinak <yroslavbot1@gmail.com>
2026-08-14 18:28:53 +0300

    Migration: Train period to year and month

---
+++ DB/Migrations/20260814152610_TrainPeriodToYearMonth.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrainPeriodToYearMonth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Period",
+                table: "Trains");
+
+            migrationBuilder.AddColumn<int>(
+                name: "month",
+                table: "Trains",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "year",
+                table: "Trains",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "month",
+                table: "Trains");
+
+            migrationBuilder.DropColumn(
+                name: "year",
+                table: "Trains");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Period",
+                table: "Trains",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
