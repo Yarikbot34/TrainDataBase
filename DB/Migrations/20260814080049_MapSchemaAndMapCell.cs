0f338c48cba9704111550832208ae19c3043861e
Kozinak <yroslavbot1@gmail.com>
2026-08-14 11:04:55 +0300

    Migration: Add mapSchema and mapCell table

---
+++ DB/Migrations/20260814080049_MapSchemaAndMapCell.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class MapSchemaAndMapCell : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MapSchemas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MapSchemas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MapCells",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SchemaId = table.Column<int>(type: "integer", nullable: false),
+                    CellId = table.Column<string>(type: "text", nullable: false),
+                    Shape = table.Column<string>(type: "text", nullable: false),
+                    Load = table.Column<int>(type: "integer", nullable: true),
+                    Label = table.Column<string>(type: "text", nullable: true),
+                    x = table.Column<int>(type: "integer", nullable: true),
+                    y = table.Column<int>(type: "integer", nullable: true),
+                    Source = table.Column<string>(type: "text", nullable: true),
+                    Target = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MapCells", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MapCells_MapSchemas_SchemaId",
+                        column: x => x.SchemaId,
+                        principalTable: "MapSchemas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MapCells_SchemaId",
+                table: "MapCells",
+                column: "SchemaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MapCells");
+
+            migrationBuilder.DropTable(
+                name: "MapSchemas");
+        }
+    }
+}
