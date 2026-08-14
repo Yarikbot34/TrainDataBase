d36caf2bfaec0bca1693129b94c224222103c22f
Kozinak <yroslavbot1@gmail.com>
2026-08-14 10:31:34 +0300

    Create: MapShema class

---
+++ Domain/Classes/MapSchema.cs
@@ -0,0 +1,9 @@
+namespace Domain.Classes;
+
+public class MapSchema
+{
+    public int Id { get; set; } 
+    public string Name { get; set; }
+    public  string Description { get; set; }
+    List<MapCell> MapCells { get; set; }
+}
