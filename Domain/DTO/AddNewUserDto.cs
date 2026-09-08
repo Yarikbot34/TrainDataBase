b8c39b196a8b37d8ea57190ff762f40d27c63c49
Kozinak <yroslavbot1@gmail.com>
2026-09-08 20:51:39 +0300

    Create: User register dto

---
+++ Domain/DTO/AddNewUserDto.cs
@@ -0,0 +1,10 @@
+namespace Domain.DTO;
+
+public class AddNewUserDto
+{
+    public string AdminPassword {get; set;}
+    
+    public string Name {get; set;}
+    public string Role {get; set;}
+    public string Password {get; set;}
+}
